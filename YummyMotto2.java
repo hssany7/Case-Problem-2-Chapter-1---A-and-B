4170b29c8aab043032b947cd3abdac5851ea973e
hssany7 <144744108+hssany7@users.noreply.github.com>
2025-05-07 17:54:45 -0400

    Add files via upload

---
+++ YummyMotto2.java
@@ -0,0 +1,10 @@
+// YummyMotto2.java
+// Displays Yummy Catering's motto with a border of asterisks
+
+public class YummyMotto2 {
+    public static void main(String[] args) {
+        System.out.println("************************************");
+        System.out.println("* Yummy makes the food that makes it a party. *");
+        System.out.println("************************************");
+    }
+}
